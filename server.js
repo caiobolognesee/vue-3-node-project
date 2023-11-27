6da439da472009ce5caf6836685e5eac6e44878f
Caio Bolognese <bolognese8891@hotmail.com>
2023-11-27 13:26:09 -0300

    add cors configuration

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import app from './src/app.js';
+import cors from 'cors';
 
 const PORT = 3001;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log('Executando');
